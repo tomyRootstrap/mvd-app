852672510ac50bd7109036896dee9c59944d3183
tomyRootstrap <tomyleonangeli@gmail.com>
2022-03-22 09:15:59 -0300

    fixing the previous commit

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,6 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './index.css';
 const MapView = () => {
   return (
     <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13}>
