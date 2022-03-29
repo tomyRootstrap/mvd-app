52d2467854a7446857bd49c41998b697f9c225c6
tomyRootstrap <tomyleonangeli@gmail.com>
2022-03-29 11:19:29 -0300

    add geolocation permission

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,50 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './style.css';
+import { useEffect, useState } from 'react';
+import myIcon from './Icon';
 const MapView = () => {
-  return (
-    <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13}>
+  const [currentPosition, setCurrentPosition] = useState({
+    ready: false,
+    where: [],
+    error: null,
+  });
+  const geoOptions = {
+    enableHighAccuracy: true,
+    timeout: 2000,
+    maximumAge: 0,
+  };
+  const geolocationEnabled = () => {
+    setCurrentPosition({ ready: false, error: null });
+    return navigator.geolocation.getCurrentPosition(
+      position => {
+        geoSuccess(position);
+      },
+      () => {},
+      geoOptions
+    );
+  };
+
+  const geoSuccess = position => {
+    setCurrentPosition({
+      ready: true,
+      where: [position.coords.latitude, position.coords.longitude],
+    });
+  };
+
+  useEffect(() => {
+    return geolocationEnabled();
+  }, []);
+
+  return currentPosition.where?.length === 2 ? (
+    <MapContainer center={currentPosition.where} zoom="20" scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker position={currentPosition.where} icon={myIcon} />
     </MapContainer>
-  );
+  ) : null;
 };
 
 export default MapView;
